78f86dd5f73f78132d35a88ec7d3f4db6ee30505
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-19 16:21:27 +0530

    Add files via upload

---
+++ JavaScript/java.js
@@ -0,0 +1,11 @@
+const student = {
+    fullname :"Aniket",
+    age : 20,
+    cgpa : 7.54,
+    isPass : true,
+};
+
+console.log(student.fullname);
+console.log(student.age);
+console.log(student.cgpa);
+console.log(student.isPass);
