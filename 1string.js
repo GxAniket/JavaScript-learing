bb98ea1765657d12638e3303961805ba92a278aa
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-21 21:22:19 +0530

    Create 1string.js

---
+++ 1string.js
@@ -0,0 +1,7 @@
+let str = "HelloWorld";
+        let size = 0;
+        for (let i of str) {
+            document.write(i);
+            size++;
+            document.write(" size = " + size +  ")<br>");
+        }
