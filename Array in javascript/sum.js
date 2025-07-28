1ce6a2cbfab4b5bb1746994d09e008161738a743
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-29 00:13:14 +0530

    Create sum.js

---
+++ Array in javascript/sum.js
@@ -0,0 +1,7 @@
+// find total no of marks in array 
+let marks = [80, 25, 38, 49, 55];
+let sum = 0;
+for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+}
+console.log("Sum of marks:", sum);
