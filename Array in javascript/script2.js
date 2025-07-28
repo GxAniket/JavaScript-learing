69c4189b003dc15adcec86b674ca78acc76d118b
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-28 20:39:23 +0530

    Create script2.js

---
+++ Array in javascript/script2.js
@@ -0,0 +1,15 @@
+let marks = [10, 20, 30, 40, 50];
+
+console.log(marks);
+
+marks.push(60);
+
+console.log(marks);
+
+console.log(marks.length);
+
+let students = ["Alice", "Bob", "Charlie" , "David" , "Eve"];
+
+console.log(students);
+
+console.log(typeof students);
